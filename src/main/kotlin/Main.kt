924207bebee822ade7ef318b321edf09a8ce9791
CamiloCapador <juanka1307@gmail.com>
2023-08-29 13:22:31 -0500

    Correccion Taller

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,5 @@
 import java.sql.SQLOutput
+import java.text.DecimalFormat
 
 fun main(args: Array<String>) {
 
@@ -6,24 +7,37 @@ fun main(args: Array<String>) {
     //700935
 
     println("Quiz 1")
-    println("\n1. Ejercicio Transporte")
-    println("2. Ejecicio Notas")
+    var opc = 0
+    var notas = arrayListOf<Double>()
+    var notasMenores = arrayListOf<Double>()
 
-    println("\nDigite opcion: ")
+    while (opc != 3){
 
-    var salir = false
+        println("""
+            
+        1. Ejercicio transporte
+        2. Ejercicio notas
+        3. Salir
+        
+        Digite opcion: 
+        """.trimIndent())
 
-    while (!salir){
-        var opc = readln().toInt()
+        opc = readln().toInt()
+
         when (opc){
             1->{
-                var tarifa = 8500
-                println("Digite número de trayectos: ")
-                val nTrayectos = readln().toInt()
-                println("Digite su edad: ")
-                val edad = readln().toInt()
+                val tarifa = 8500
+                println("""
+                    
+                    Ejercicio Trayectos 
 
-                val totalViaje = (tarifa * nTrayectos)
+                    Digite número de trayectos: 
+                """.trimIndent())
+                var nTrayectos = readln().toInt()
+                println("Digite su edad: ")
+                var edad = readln().toInt()
+
+                var totalViaje = (tarifa * nTrayectos)
                 if (edad > 59 ){
                     println("""
                         
@@ -38,30 +52,76 @@ fun main(args: Array<String>) {
                     var descuento = readln().toInt()
                     if (descuento == 1){
                         val total = (totalViaje * 0.45)
-                        println("Su total a pagar por el viaje con el 45% de Dto es de $$total")
+                        println("\nSu total a pagar por el viaje con el 45% de Dto es de $$total")
                     } else {
-                        println("Su total a pagar por el viaje sin Dto es de $$totalViaje")
+                        println("\nSu total a pagar por el viaje sin Dto es de $$totalViaje")
                     }
                 } else if (edad < 60){
-                    println("Su total a pagar por el viaje es de $$totalViaje")
+                    println("\nSu total a pagar por el viaje es de $$totalViaje")
                 }
             }
             2-> {
-                /*println("Digite número de notas")
-                val nNotas = readln().toInt()*/
-
-                val notas = arrayOf(3.5 , 2.0 , 40 , 38 )
-
                 println("""
-                    1. Promedio Notas
-                    2. Nota mas alta
-                    3. Nota mas baja
-                    Digite opcion: 
+                    
+                    Ejercicio Notas
+                    
+                    Digite número de notas:
                 """.trimIndent())
+                var nNotas = readln().toInt()
+                var nota = 0.0
 
-                var opcNota = readln().toInt()
+                for(i in 0 until nNotas) {
+                    println("Ingrese nota ${i + 1}: ")
+                    nota = readln().toDouble()
+                    notas.add(nota)
+                }
+
+                if (nota < 35){
+                    notasMenores.add(nota)
+                }
+                println("""
+                    
+                    Que desea realizar:
+                    
+                    1. Promedio
+                    2. Calificación mas alta
+                    3. Calificación mas baja 
+                    4. Promedio calificacion menor a 3.5
+                    
+                    Digite opción: 
+                """.trimIndent())
+                var opcNotas = readln().toInt()
+
+                when (opcNotas){
+                    1-> {
+                        var suma:Double = 0.0
+                        for (i in 0 until notas.size){
+                            suma += notas [i]
+                        }
+                        var promedio = (suma / notas.size)
+                        println(DecimalFormat("EL promedio de las notas es #.#").format(promedio))
+                    }
+                    2-> {
+                        var notaMayor = notas.max()
+                        println("La nota mas alta es $notaMayor")
+                    }
+                    3-> {
+                        var notaMenor = notas.min()
+                        println("La nota mas baja es $notaMenor")
+                    }
+                    4-> {
+                        var sumaMenores:Double = 0.0
+                        for (i in 0 until notasMenores.size){
+                            sumaMenores += notasMenores [i]
+                        }
+                        var promedioMenores = (sumaMenores / notasMenores.size)
+                        println(DecimalFormat("EL promedio de las notas menores a 3.5 es #.#").format(promedioMenores))
+                    }
+                    else -> println("Opción incorrecta")
+                }
             }
-            else-> println("Adios")
+            3-> println("Adios")
+            else-> println("\nOpcion incorrecta")
         }
     }
 }
